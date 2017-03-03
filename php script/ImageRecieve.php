679e1d7cf2a7b84b4f69cf2fcf2688fedcce81c4
Shubham Vashisht <vashisht.s21295@gmail.com>
2017-03-03 09:36:27 +0530

    php script added

---
+++ php script/ImageRecieve.php
@@ -0,0 +1,52 @@
+<?php
+ 
+// Path to move uploaded files
+$target_path = "recieved/";
+ 
+// array for final json respone
+$response = array();
+ 
+// getting server ip address
+$server_ip = gethostbyname(gethostname());
+ 
+// final file url that is being uploaded
+$file_upload_url = 'http://' . $server_ip . '/' . 'AndroidFileUpload' . '/' . $target_path;
+ 
+ 
+if (isset($_FILES['image']['name'])) {
+    $target_path = $target_path . basename($_FILES['image']['name']);
+ 
+    // reading other post parameters
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $website = isset($_POST['website']) ? $_POST['website'] : '';
+ 
+    $response['file_name'] = basename($_FILES['image']['name']);
+    $response['email'] = $email;
+    $response['website'] = $website;
+ 
+    try {
+        // Throws exception incase file is not being moved
+        if (!move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
+            // make error flag true
+            $response['error'] = true;
+            $response['message'] = 'Could not move the file!';
+        }
+ 
+        // File successfully uploaded
+        $response['message'] = 'File uploaded successfully!';
+        $response['error'] = false;
+        $response['file_path'] = $file_upload_url . basename($_FILES['image']['name']);
+    } catch (Exception $e) {
+        // Exception occurred. Make error flag true
+        $response['error'] = true;
+        $response['message'] = $e->getMessage();
+    }
+} else {
+    // File parameter is missing
+    $response['error'] = true;
+    $response['message'] = 'Not received any file!F';
+}
+ 
+// Echo final json response to client
+echo json_encode($response);
+?>
